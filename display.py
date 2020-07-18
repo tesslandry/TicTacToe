198783f16763cc05ee673d20ba007e418db37d81
Tess Landry <tesslalandry@gmail.com>
2020-07-18 15:22:48 -0400

    Board is now drawn

--- display.py
+++ display.py
@@ -2,14 +2,43 @@
 
 import pygame
 import sys
+import board
 pygame.init()
 
-screen = pygame.display.set_mode((800,800))
+#COLORS
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+CYAN  = (0, 255, 255)
+
+#DISPLAY
 pygame.display.set_caption('Tic Tac Toe')
+SCREEN_WIDTH = 800
+SCREEN_SIZE = (800, 800)
+BACKGROUND_COLOR = CYAN
+
+
+screen = pygame.display.set_mode(SCREEN_SIZE)
+
+def drawBoard():
+        screen.fill(BACKGROUND_COLOR)
+
+        #We want our board to draw based on screen size
+        #We want margins on the outside
+        spacing = SCREEN_WIDTH / 5
+        lineLength = spacing*3
+
+        #Vertical Lines
+        pygame.draw.line(screen, BLACK, (spacing*2, spacing), (spacing*2, (spacing + lineLength)), 2)
+        pygame.draw.line(screen, BLACK, (spacing*3, spacing), (spacing*3, (spacing + lineLength)), 2) 
+        #Horizontal Lines
+        pygame.draw.line(screen, BLACK, (spacing, spacing*2), ((spacing + lineLength), spacing*2), 2)
+        pygame.draw.line(screen, BLACK, (spacing, spacing*3), ((spacing + lineLength), spacing*3), 2)      
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-        screen.fill((0,255,255))
+        drawBoard()
+
         pygame.display.flip()
---
+++ main.py
@@ -0,0 +1,7 @@
+#Main
+
+def main():
+    #Put our main running code here
+
+if __name__ == "__main__":
+    main()
